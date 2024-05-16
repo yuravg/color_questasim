16633873c62cfc2892548a35c622389accf9b0de
Yuriy VG <yuravg@gmail.com>
2024-05-16 20:16:39 +0300

    Add test
    Add a test to check if an invalid options message is issued for the
    'vsim' command.

--- test/test_vsim_uvm.sh
+++ test/test_vsim_uvm.sh
@@ -9,5 +9,8 @@ title() {
 title "example_uvm_tb.sv"
 vlog -incr -lint -sv +acc -timescale 1ns/1ps -quiet example_uvm_tb.sv && vsim -c example_uvm_tb -do 'run -all; quit'
 
+title "incorrect options"
+vlog -incr -lint -sv +acc -timescale 1ns/1ps -quiet example_uvm_tb.sv && vsim +UVM_VERBOSITY=TMP -c example_uvm_tb -do 'run -all; quit'
+
 title "example_uvm_tb.sv with +uvm_no_relnotes"
 vlog -incr -lint -sv +acc -timescale 1ns/1ps -quiet example_uvm_tb.sv && vsim +UVM_NO_RELNOTES -c example_uvm_tb -do 'run -all; quit'
